e7ecaec547942ec4963de477ffb5c41ecf7163ae
wjd <2817453937@qq.com>
2020-11-09 14:24:25 +0800

    first commit

---
+++ factorial/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,31 @@
+﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <iomanip> 
+#include <stdio.h> 
+#include<math.h>
+#include<cstdlib>
+using namespace std;
+double factorial(int n);
+int main()
+{
+	int n;
+	double y;
+
+	scanf("%d",&n);
+	y = factorial(n);
+	printf("%f",y);
+
+}
+double factorial(int n)
+{
+	double sum = 1;
+	for (int i = 1; i <= n; i++)
+	{
+
+		sum *= i;
+	}
+	return sum;
+
+}
